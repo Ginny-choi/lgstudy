76e2a87c7b2c4901d4243873f4147dca2f478df9
DESKTOP-MDBS3BI\badacho <choieeejeong@gmail.com>
2021-11-10 11:35:21 +0900

    헤더 웹&모바일 스크립트 제작

---
+++ assets/js/main.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+	const gnbNav = $(".gnb-nav");
+	const mobileGnbMenu = $(".mobile-gnb-menu");
+	const webDeps2 = $(".deps2");
+
+	const mainGnb = $(".main-gnb");
+	const mobileBtn = $(".mobile-btn");
+	const mobileGnbBtn = $(".mobile-gnbBtn");
+	const mgnbCloseBtn = $(".mgnb-closeBtn");
+
+	const languageBtn = $(".language.kor");
+	const btnWrap = $(".btn-wrap");
+
+	mainGnb.on({
+		mouseenter: function () {
+			webDeps2.hide(200);
+			$(this).next().show(200);
+		},
+	});
+	gnbNav.on({
+		mouseleave: function () {
+			webDeps2.hide(200);
+		},
+	});
+
+	mobileBtn.on({
+		click: function (e) {
+			e.preventDefault();
+			mobileGnbMenu.show();
+		},
+	});
+
+	mobileGnbBtn.on({
+		click: function (e) {
+			e.preventDefault();
+			$(this).toggleClass("active");
+			$(this).next().slideToggle(300);
+		},
+	});
+
+	mgnbCloseBtn.on({
+		click: function (e) {
+			e.preventDefault();
+			mobileGnbMenu.hide();
+		},
+	});
+
+	languageBtn.on({
+		click: function () {
+			btnWrap.toggleClass("on");
+		},
+	});
+});
